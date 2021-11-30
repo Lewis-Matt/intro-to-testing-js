b3afaf9ed99d3d8878b31a506b9220518562d948
Matthew Lewis <matthew.d.lewis64@gmail.com>
2021-11-30 15:17:40 -0600

    Wrote unit test for sayHello(), refactored sayHello

--- code.js
+++ code.js
@@ -4,4 +4,17 @@ const helloWorld = function () {
 }
 
 // sayHello function
-function sayHello(){};
+// ORIGINAL
+// function sayHello(input) {
+//     if (input === 'Alex') {
+//         return 'Hello, ' + input;
+//     } else if (input === 'Pat') {
+//         return 'Hello, ' + input;
+//     } else {
+//         return 'Hello, Jane';
+//     }
+// }
+
+// REFACTORED
+let sayHello = input => `Hello, ${input}`;
+
